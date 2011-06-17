fdc264c7c4f20e4b6643fac3084f40d24c401dcc
Heungsub Lee <heung@sublee.kr>
2011-06-17 22:45:36 +0900

    테스트 돌아가도록 수정

--- test.py
+++ test.py
@@ -3,10 +3,30 @@ import unittest
 import josa
 
 
-class HangulPostpositionTestCase(unittest.TestCase):
+class JosaTestCase(unittest.TestCase):
 
-    def runTest(self):
-        pass
+    def test_jongseong_checking(self):
+        assert josa.has_jongseong('game')
+        assert josa.has_jongseong('lykit')
+        assert josa.has_jongseong('kijun')
+        assert josa.has_jongseong('incl')
+        assert josa.has_jongseong('nexon')
+        assert josa.has_jongseong('python')
+        #assert josa.has_jongseong('facebook')
+        assert josa.has_jongseong('hangul')
+        assert josa.has_jongseong('gothic')
+        #assert josa.has_jongseong('google')
+        #assert josa.has_jongseong('apple')
+        assert josa.has_jongseong('aladin')
+        assert not josa.has_jongseong('hangulize')
+        assert not josa.has_jongseong('josa')
+        assert not josa.has_jongseong('sublee')
+        assert not josa.has_jongseong('dahlia')
+        assert not josa.has_jongseong('shinvee')
+        assert not josa.has_jongseong('java')
+        assert not josa.has_jongseong('kart')
+        assert not josa.has_jongseong('rider')
+        assert not josa.has_jongseong('architecture')
 
     def test_josa_english(self):
         assert u'를' == josa.josa(u'false positive', u'를')
@@ -16,9 +36,9 @@ class HangulPostpositionTestCase(unittest.TestCase):
 
     def test_append_english(self):
         assert u'false positive를' == josa.append(u'false positive', u'를')
-        assert u'false negative' == josa.append(u'false negative', u'을')
+        assert u'false negative를' == josa.append(u'false negative', u'을')
         assert u'function이' == josa.append(u'function', u'가')
-        assert u'deterministic이랑' == josa.josa(u'deterministic', u'랑')
+        assert u'deterministic이랑' == josa.append(u'deterministic', u'랑')
 
     def test_josa_korean(self):
         assert u'를' == josa.josa(u'택시', u'를', lang='kor')
@@ -47,6 +67,7 @@ class HangulPostpositionTestCase(unittest.TestCase):
 
 
 def suite():
+    loader = unittest.TestLoader()
     suite = unittest.TestSuite()
-    suite.addTest(HangulPostpositionTestCase())
+    suite.addTests(loader.loadTestsFromTestCase(JosaTestCase))
     return suite
